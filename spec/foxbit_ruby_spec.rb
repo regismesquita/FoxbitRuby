7b60956dabe4c81bca35d7dc2678725cde88853d
rdsm <rdsm@posteo.net>
2016-07-02 01:33:04 -0300

    Fixing terms

--- spec/foxbit_ruby_spec.rb
+++ spec/foxbit_ruby_spec.rb
@@ -6,18 +6,18 @@ RSpec.describe "FoxbitRuby#orders" do
               .to_return(body: '{"pair":"BTCBRL","bids":[[2000.0,0.01,90804599]],"asks":[[2000.00,0.25,90855278]]}')
   end
 
-  it "should return an order list with asks and bids" do
+  it "return an order list with asks and bids" do
     fbr = FoxbitRuby.new
     expect(fbr.orders[:asks].first.class).to eq(Order)
   end
 
-  it "should parse asks correctly" do
+  it "parse asks correctly" do
     fbr = FoxbitRuby.new
     ask = fbr.orders[:asks].first
     expect([ask.price,ask.bitcoin_amount,ask.id]).to eq([2000.00,0.25,90855278])
   end
 
-  it "should parse asks correctly" do
+  it "parse bids correctly" do
     fbr = FoxbitRuby.new
     bid = fbr.orders[:bids].first
     expect([bid.price,bid.bitcoin_amount,bid.id]).to eq([2000.0,0.01,90804599])
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,35 +1,35 @@
 require 'order'
 
-RSpec.describe 'Order' do
+RSpec.describe Order do
   describe '.order_from_api' do
     before(:each) do
-      @orders = Order.orders_from_api(bids: [[2000,0.01,1],[1000,0.02,2]], asks: [[1200,0.01,3],[1300,0.02,4]])
+      @orders = described_class.orders_from_api(bids: [[2000,0.01,1],[1000,0.02,2]], asks: [[1200,0.01,3],[1300,0.02,4]])
     end
-    it 'should receive receive bids and asks from api and return an array of orders' do
+    it 'receive bids and asks from api and return an array of orders' do
       first_bid_order = @orders[:bids].first
       parameter_list = [first_bid_order.price, first_bid_order.bitcoin_amount, first_bid_order.id]
       expect(parameter_list).to eq([2000,0.01,1])
     end
 
-    it 'should load mutiple bids' do
+    it 'load mutiple bids' do
       expect(@orders[:bids].size).to eq(2)
     end
 
-    it 'should load mutiple asks' do
+    it 'load mutiple asks' do
       expect(@orders[:asks].size).to eq(2)
     end
 
   end
 
   describe '#fiat_value' do
-    it 'should return the fiat value of the order' do
+    it 'return the fiat value of the order' do
       order = Order.new(price: 200, bitcoin_amount: 1.5)
       expect(order.fiat_value).to eq(300)
     end
   end
 
   describe '#satoshis' do
-    it 'should return the bitcoin amount in satoshis' do
+    it 'return the bitcoin amount in satoshis' do
       order = Order.new(bitcoin_amount: 1.5)
       expect(order.satoshis).to eq(150_000_000)
     end
