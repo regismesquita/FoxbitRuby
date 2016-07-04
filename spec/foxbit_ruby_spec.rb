492c5c4e2d832dfdbb80068070537f6912dbaa91
rdsm <rdsm@posteo.net>
2016-07-04 11:38:50 -0300

    specs refactoring

--- spec/foxbit_ruby_spec.rb
+++ spec/foxbit_ruby_spec.rb
@@ -1,25 +1,24 @@
-RSpec.describe "FoxbitRuby#orders" do
-  before do
-    stub = stub_request(:get, 'https://api.blinktrade.com/api/v1/BRL/orderbook')
-              .to_return(body: '{"pair":"BTCBRL","bids":[[2000.0,0.01,90804599]],"asks":[[2000.00,0.25,90855278]]}')
+RSpec.describe FoxbitRuby do
+  describe '#orders' do
+    before do
+      stub = stub_request(:get, 'https://api.blinktrade.com/api/v1/BRL/orderbook')
+        .to_return(body: '{"pair":"BTCBRL","bids":[[2000.0,0.01,90804599]],"asks":[[2000.00,0.25,90855278]]}')
+    end
+
+    subject(:FoxbitRuby) { described_class.new }
+
+    it "return an order list with asks and bids" do
+      expect(subject.orders[:asks].first.class).to eq(Order)
+    end
+
+    it "parse asks correctly" do
+      ask = subject.orders[:asks].first
+      expect([ask.price,ask.bitcoin_amount,ask.id]).to eq([2000.00,0.25,90855278])
+    end
+
+    it "parse bids correctly" do
+      bid = subject.orders[:bids].first
+      expect([bid.price,bid.bitcoin_amount,bid.id]).to eq([2000.0,0.01,90804599])
+    end
   end
-
-  it "return an order list with asks and bids" do
-    fbr = FoxbitRuby.new
-    expect(fbr.orders[:asks].first.class).to eq(Order)
-  end
-
-  it "parse asks correctly" do
-    fbr = FoxbitRuby.new
-    ask = fbr.orders[:asks].first
-    expect([ask.price,ask.bitcoin_amount,ask.id]).to eq([2000.00,0.25,90855278])
-  end
-
-  it "parse bids correctly" do
-    fbr = FoxbitRuby.new
-    bid = fbr.orders[:bids].first
-    expect([bid.price,bid.bitcoin_amount,bid.id]).to eq([2000.0,0.01,90804599])
-  end
-
-
 end
--- spec/order_spec.rb
+++ spec/order_spec.rb
@@ -1,35 +1,46 @@
 RSpec.describe Order do
   describe '.order_from_api' do
-    before(:each) do
-      @orders = described_class.orders_from_api(bids: [[2000,0.01,1],[1000,0.02,2]], asks: [[1200,0.01,3],[1300,0.02,4]])
-    end
+    subject(:orders){ described_class.orders_from_api(bids: [[2000,0.01,1],[1000,0.02,2]], asks: [[1200,0.01,3],[1300,0.02,4]]) }
+
     it 'receive bids and asks from api and return an array of orders' do
-      first_bid_order = @orders[:bids].first
+      first_bid_order = subject[:bids].first
       parameter_list = [first_bid_order.price, first_bid_order.bitcoin_amount, first_bid_order.id]
       expect(parameter_list).to eq([2000,0.01,1])
     end
 
     it 'load mutiple bids' do
-      expect(@orders[:bids].size).to eq(2)
+      expect(subject[:bids].size).to eq(2)
     end
 
     it 'load mutiple asks' do
-      expect(@orders[:asks].size).to eq(2)
+      expect(subject[:asks].size).to eq(2)
     end
 
   end
 
   describe '#fiat_value' do
+    subject(:order){ described_class.new(price: 200, bitcoin_amount: 1.5) }
+
     it 'return the fiat value of the order' do
-      order = Order.new(price: 200, bitcoin_amount: 1.5)
-      expect(order.fiat_value).to eq(300)
+      expect(subject.fiat_value).to eq(300)
+    end
+
+    it 'updates the fiat value when bitcoin amount changes' do
+      subject.bitcoin_amount = 2
+      expect(subject.fiat_value).to eq(400)
+    end
+
+    it 'updates the fiat value when price changes' do
+      subject.price = 300
+      expect(subject.fiat_value).to eq(450)
     end
   end
 
   describe '#satoshis' do
+    subject(:order){ described_class.new(bitcoin_amount: 1.5) }
+
     it 'return the bitcoin amount in satoshis' do
-      order = Order.new(bitcoin_amount: 1.5)
-      expect(order.satoshis).to eq(150_000_000)
+      expect(subject.satoshis).to eq(150_000_000)
     end
   end
 end
