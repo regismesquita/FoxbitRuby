72dad87478e641515479091ee1fc13df2f9a9265
rdsm <rdsm@posteo.net>
2016-07-04 12:23:01 -0300

    FoxbitRuby subject to the top of spec

--- spec/foxbit_ruby_spec.rb
+++ spec/foxbit_ruby_spec.rb
@@ -1,11 +1,11 @@
 RSpec.describe FoxbitRuby do
+  subject(:FoxbitRuby) { described_class.new }
+
   describe '#orders' do
     before do
       stub = stub_request(:get, 'https://api.blinktrade.com/api/v1/BRL/orderbook')
         .to_return(body: '{"pair":"BTCBRL","bids":[[2000.0,0.01,90804599]],"asks":[[2000.00,0.25,90855278]]}')
     end
-
-    subject(:FoxbitRuby) { described_class.new }
 
     it "return an order list with asks and bids" do
       expect(subject.orders[:asks].first.class).to eq(Order)
