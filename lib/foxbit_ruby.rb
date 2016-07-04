731c7a76ac998456d2f500383f520344d9495423
rdsm <rdsm@posteo.net>
2016-07-04 12:20:28 -0300

    instant sell price

--- lib/foxbit_ruby.rb
+++ lib/foxbit_ruby.rb
@@ -10,6 +10,18 @@ class FoxbitRuby
     Order.orders_from_api(parsed_json_orders)
   end
 
+  def instant_sell_price(amount_to_sell=0)
+    price_ordered_list = orders[:bids].sort{|c,d| c.price <=> d.price}.reverse
+    current_amount = 0
+    while
+      current_order = price_ordered_list.shift
+      current_amount += current_order.bitcoin_amount
+      break if current_amount >= amount_to_sell
+    end
+    raise 'Amount not available' if current_order.nil?
+    current_order.price
+  end
+
   private
 
   def api_data
--- spec/foxbit_ruby_spec.rb
+++ spec/foxbit_ruby_spec.rb
@@ -21,4 +21,23 @@ RSpec.describe FoxbitRuby do
       expect([bid.price,bid.bitcoin_amount,bid.id]).to eq([2000.0,0.01,90804599])
     end
   end
+  describe '#instant_sell_price' do
+    before do
+      stub = stub_request(:get, 'https://api.blinktrade.com/api/v1/BRL/orderbook')
+        .to_return(body: '{"pair":"BTCBRL","bids":[[2000.0,0.01,90804599],[1800.0,0.04,90904599],[1600.0,0.03,90904599]],"asks":[[2000.00,0.25,90855278]]}')
+    end
+
+    it 'returns biggest bid price in order book (so we can sell)' do
+      expect(subject.instant_sell_price).to eq(2000)
+    end
+
+    it 'returns biggest bid price for the amount we want to sell.' do
+      expect(subject.instant_sell_price(0.05)).to eq(1800)
+    end
+
+    context 'given an amount not available'
+    it "throw an 'amount not available' exception" do
+      expect{subject.instant_sell_price(1.00)}.to raise_exception('Amount not available')
+    end
+  end
 end
